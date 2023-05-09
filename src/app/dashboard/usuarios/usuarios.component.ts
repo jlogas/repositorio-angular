50b5f07bb2c86284b2c8311750f1851b0bde32a3
jlogas <juanjlogas@gmail.com>
2023-05-09 10:08:55 -0500

    servicio usuarios

--- src/app/dashboard/usuarios/usuarios.component.ts
+++ src/app/dashboard/usuarios/usuarios.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Usuario } from 'src/app/interfaces/usuarios';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 
 
@@ -11,18 +12,24 @@ import { Usuario } from 'src/app/interfaces/usuarios';
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
-export class UsuariosComponent {
-  listaUsuarios: Usuario[] = [
-    {usuario: "jjuan", nombre: "juan jose", apellido: "lopez", email:"juanjlopez@gmail.com"},
-    {usuario: "jema", nombre: "julio emanuel", apellido: "venegas", email:"juliovene@gmail.com"},
-    {usuario: "sofigale", nombre: "sofia", apellido: "galeano", email:"sofiagale@gmail.com"},
-    {usuario: "aesteban", nombre: "andres esteban", apellido: "corzo", email:"andrescorzo@gmail.com"},
-  
-  ];
+export class UsuariosComponent  implements OnInit{
+
+  listUusarios: Usuario[]= [];
+
+
   displayedColumns: string[] = ['usuario', 'nombre', 'apellido', 'email','acciones'];
-  dataSource = new MatTableDataSource(this.listaUsuarios);
+  dataSource : MatTableDataSource<any>;
 
+constructor( private _usuarioService: UsuarioService){
 
+}
+  ngOnInit(): void {
+    this.cargarUsuarior()
+  }
+  cargarUsuarior(){
+    this.listUusarios = this._usuarioService.getUsuario();
+    this.dataSource = new MatTableDataSource(this.listUusarios)
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
---
+++ src/app/servicios/usuario.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from '../interfaces/usuarios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  listaUsuarios: Usuario[] = [
+    {usuario: "jjuan", nombre: "juan jose", apellido: "lopez", email:"juanjlopez@gmail.com"},
+    {usuario: "jema", nombre: "julio emanuel", apellido: "venegas", email:"juliovene@gmail.com"},
+    {usuario: "sofigale", nombre: "sofia", apellido: "galeano", email:"sofiagale@gmail.com"},
+    {usuario: "aesteban", nombre: "andres esteban", apellido: "corzo", email:"andrescorzo@gmail.com"},
+  
+  ];
+
+  constructor() { }
+
+  getUsuario(){
+    return this.listaUsuarios.slice();
+  }
+}
